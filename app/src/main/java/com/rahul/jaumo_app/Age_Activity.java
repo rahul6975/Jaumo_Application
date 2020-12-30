3b4279d840055f880f856539784973ca021b1b38
sarika1609 <sarika.thota1992@gmail.com>
2020-12-30 13:51:10 +0530

    merged till home screen

--- app/src/main/java/com/rahul/jaumo_app/Age_Activity.java
+++ app/src/main/java/com/rahul/jaumo_app/Age_Activity.java
@@ -9,6 +9,6 @@ public class Age_Activity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_gender);
+        setContentView(R.layout.activity_age_);
     }
 }

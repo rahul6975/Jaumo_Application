269e182eb971db9d8978b7eae57ed75bca9da734
rahul6975 <rahulya7569@gmail.com>
2020-12-30 14:13:11 +0530

    working on home page

---
+++ app/src/main/java/com/rahul/jaumo_app/Home_Page.java
@@ -0,0 +1,38 @@
+package com.rahul.jaumo_app;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.res.TypedArray;
+import android.media.Image;
+import android.os.Bundle;
+import android.widget.ImageView;
+
+import java.util.Random;
+
+public class Home_Page extends AppCompatActivity {
+
+    private ImageView ivRandomImage;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home__page);
+
+        ivRandomImage = findViewById(R.id.ivRandomPeoples);
+
+
+
+    }
+
+    private  void setRandomImage()
+    {
+        final TypedArray imgs = getResources().obtainTypedArray(R.array.men_images);
+        final Random rand = new Random();
+        final int randInt = rand.nextInt(imgs.length());
+        final int resID = imgs.getResourceId(randInt,0);
+
+        ivRandomImage.setImageResource(resID);
+
+
+    }
+}

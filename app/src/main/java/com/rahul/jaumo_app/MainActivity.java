6ca8ae77943c0cfa59fae0a386d5e303c4dd3850
rahul6975 <rahulya7569@gmail.com>
2020-12-31 18:58:00 +0530

    Merge conflit fixed

--- app/src/main/java/com/rahul/jaumo_app/MainActivity.java
+++ app/src/main/java/com/rahul/jaumo_app/MainActivity.java
@@ -9,12 +9,6 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-<<<<<<< HEAD
-
-        setContentView(R.layout.activity_main);
-
-=======
         setContentView(R.layout.activity_start__page);
->>>>>>> 0b2345e7330cef91cd2656e941e1a96241f971c9
     }
 }

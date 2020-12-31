9a1764c973ec8482ba0222a579804cbc8a63671c
rahul6975 <rahulya7569@gmail.com>
2020-12-31 12:43:52 +0530

    Connected everything

--- app/src/main/java/com/rahul/jaumo_app/Age_Activity.java
+++ app/src/main/java/com/rahul/jaumo_app/Age_Activity.java
@@ -2,13 +2,28 @@ package com.rahul.jaumo_app;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Age_Activity extends AppCompatActivity {
+
+    private Button btnAge;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_age_);
+
+        btnAge = findViewById(R.id.btn_ageActivity);
+
+        btnAge.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Age_Activity.this,Location.class);
+                startActivity(intent);
+            }
+        });
     }
 }

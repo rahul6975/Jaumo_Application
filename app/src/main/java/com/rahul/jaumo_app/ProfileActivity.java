57578655e58a1cd01a21b53414961e172e3bca68
rahul6975 <rahulya7569@gmail.com>
2020-12-31 18:52:09 +0530

    Added Sent Likes

--- app/src/main/java/com/rahul/jaumo_app/ProfileActivity.java
+++ app/src/main/java/com/rahul/jaumo_app/ProfileActivity.java
@@ -10,6 +10,7 @@ import android.widget.TextView;
 public class ProfileActivity extends AppCompatActivity {
 
     private TextView tvCompleteProfile;
+    private TextView tvSentLike;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,11 +18,20 @@ public class ProfileActivity extends AppCompatActivity {
         setContentView(R.layout.activity_profile);
 
         tvCompleteProfile = findViewById(R.id.tv_complete_profile);
+        tvSentLike = findViewById(R.id.tvSentLikes);
 
         tvCompleteProfile.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(ProfileActivity.this,CompleteProfileActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        tvSentLike.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(ProfileActivity.this,Sent_Activity.class);
                 startActivity(intent);
             }
         });

88c6ae6ee8e49985e036738ed67d946bf6629b9c
rahul6975 <rahulya7569@gmail.com>
2021-01-01 21:11:52 +0530

    Added list

--- app/src/main/java/com/rahul/jaumo_app/SignUpWithEmail.java
+++ app/src/main/java/com/rahul/jaumo_app/SignUpWithEmail.java
@@ -7,12 +7,14 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 public class SignUpWithEmail extends AppCompatActivity {
     EditText et_email, et_password;
     TextView tv_email, tv_password;
     Button btn_signin;
+    ImageView ibDropDownList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,6 +28,7 @@ public class SignUpWithEmail extends AppCompatActivity {
         et_email = findViewById(R.id.et_email);
         et_password = findViewById(R.id.et_password);
         tv_email = findViewById(R.id.tv_email);
+        ibDropDownList = findViewById(R.id.ivMore);
         tv_password = findViewById(R.id.tv_password);
         btn_signin = findViewById(R.id.btn_signin);
         btn_signin.setOnClickListener(new View.OnClickListener() {
@@ -37,8 +40,15 @@ public class SignUpWithEmail extends AppCompatActivity {
               }
             }
         });
-    }
 
+        ibDropDownList.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(SignUpWithEmail.this,Support.class);
+                startActivity(intent);
+            }
+        });
+    }
     private boolean isValidForm() {
         boolean isValid = true;
         if (et_email.getText().toString().isEmpty()) {

b5c3fdc187cc11296235f50da26abac1428ea0c8
sarika1609 <sarika.thota1992@gmail.com>
2021-01-01 13:38:20 +0530

    added radio button validations

--- app/src/main/java/com/rahul/jaumo_app/SelectDistanceActivity.java
+++ app/src/main/java/com/rahul/jaumo_app/SelectDistanceActivity.java
@@ -5,22 +5,144 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CompoundButton;
+import android.widget.ImageView;
+import android.widget.RadioButton;
 
-public class SelectDistanceActivity extends AppCompatActivity {
-
+public class SelectDistanceActivity extends AppCompatActivity implements CompoundButton.OnCheckedChangeListener {
+    ImageView btn_cross;
+    RadioButton rb_10,rb_25,rb_50,rb_100,rb_250,rb_500,rb_1000,rb_distance_matter;
     private Button btnSave;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_select_distance);
+        initUI();
+    }
 
-        btnSave= findViewById(R.id.btn_save);
+    private void initUI() {
+        btnSave = findViewById(R.id.btn_save);
+        btn_cross = findViewById(R.id.btn_cross);
+        rb_10=findViewById(R.id.rb_10);
+        rb_25=findViewById(R.id.rb_25);
+        rb_50=findViewById(R.id.rb_50);
+        rb_100=findViewById(R.id.rb_100);
+        rb_250=findViewById(R.id.rb_250);
+        rb_500=findViewById(R.id.rb_500);
+        rb_1000=findViewById(R.id.rb_1000);
+        rb_distance_matter=findViewById(R.id.rb_distance_matter);
+
+        rb_10.setOnCheckedChangeListener(this);
+        rb_25.setOnCheckedChangeListener(this);
+        rb_50.setOnCheckedChangeListener(this);
+        rb_100.setOnCheckedChangeListener(this);
+        rb_250.setOnCheckedChangeListener(this);
+        rb_500.setOnCheckedChangeListener(this);
+        rb_1000.setOnCheckedChangeListener(this);
+        rb_distance_matter.setOnCheckedChangeListener(this);
+
+
+        btn_cross.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
+
         btnSave.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 finish();
             }
         });
+    }
+
+    @Override
+    public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+        if (isChecked) {
+            if (buttonView.getId() == R.id.rb_10) {
+                rb_25.setChecked(false);
+                rb_50.setChecked(false);
+                rb_100.setChecked(false);
+                rb_250.setChecked(false);
+                rb_500.setChecked(false);
+                rb_1000.setChecked(false);
+                rb_distance_matter.setChecked(false);
+
+            }
+            if (buttonView.getId() == R.id.rb_25) {
+                rb_10.setChecked(false);
+                rb_50.setChecked(false);
+                rb_100.setChecked(false);
+                rb_250.setChecked(false);
+                rb_500.setChecked(false);
+                rb_1000.setChecked(false);
+                rb_distance_matter.setChecked(false);
+
+            }
+            if (buttonView.getId() == R.id.rb_50) {
+                rb_10.setChecked(false);
+                rb_25.setChecked(false);
+                rb_100.setChecked(false);
+                rb_250.setChecked(false);
+                rb_500.setChecked(false);
+                rb_1000.setChecked(false);
+                rb_distance_matter.setChecked(false);
+
+            }
+            if (buttonView.getId() == R.id.rb_100) {
+                rb_10.setChecked(false);
+                rb_25.setChecked(false);
+                rb_50.setChecked(false);
+                rb_250.setChecked(false);
+                rb_500.setChecked(false);
+                rb_1000.setChecked(false);
+                rb_distance_matter.setChecked(false);
+
+            }
+            if (buttonView.getId() == R.id.rb_250) {
+                rb_10.setChecked(false);
+                rb_25.setChecked(false);
+                rb_100.setChecked(false);
+                rb_50.setChecked(false);
+                rb_500.setChecked(false);
+                rb_1000.setChecked(false);
+                rb_distance_matter.setChecked(false);
+
+            }
+            if (buttonView.getId() == R.id.rb_500) {
+                rb_10.setChecked(false);
+                rb_25.setChecked(false);
+                rb_100.setChecked(false);
+                rb_250.setChecked(false);
+                rb_50.setChecked(false);
+                rb_1000.setChecked(false);
+                rb_distance_matter.setChecked(false);
+
+            }
+            if (buttonView.getId() == R.id.rb_1000) {
+                rb_10.setChecked(false);
+                rb_25.setChecked(false);
+                rb_100.setChecked(false);
+                rb_250.setChecked(false);
+                rb_500.setChecked(false);
+                rb_50.setChecked(false);
+                rb_distance_matter.setChecked(false);
+
+            }
+            if (buttonView.getId() == R.id.rb_distance_matter) {
+                rb_10.setChecked(false);
+                rb_25.setChecked(false);
+                rb_100.setChecked(false);
+                rb_250.setChecked(false);
+                rb_500.setChecked(false);
+                rb_50.setChecked(false);
+                rb_1000.setChecked(false);
+
+            }
+
+        }
+
     }
 }
--- app/src/main/java/com/rahul/jaumo_app/SelectGenderActivity.java
+++ app/src/main/java/com/rahul/jaumo_app/SelectGenderActivity.java
@@ -5,22 +5,65 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CompoundButton;
+import android.widget.ImageView;
+import android.widget.RadioButton;
 
-public class SelectGenderActivity extends AppCompatActivity {
-
+public class SelectGenderActivity extends AppCompatActivity implements CompoundButton.OnCheckedChangeListener {
+    ImageView btn_cross;
     private Button btnSave;
+    RadioButton rb_male,rb_female,rb_female_male;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_select_gender);
+        initUI();
+    }
 
+    private void initUI() {
         btnSave = findViewById(R.id.btn_save);
+        btn_cross=findViewById(R.id.btn_cross);
+        rb_female_male=findViewById(R.id.rb_female_male);
+        rb_female=findViewById(R.id.rb_female);
+        rb_male=findViewById(R.id.rb_male);
+
+        rb_male.setOnCheckedChangeListener(this);
+        rb_female.setOnCheckedChangeListener(this);
+        rb_female_male.setOnCheckedChangeListener(this);
+        btn_cross.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
+
         btnSave.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 finish();
             }
         });
+    }
+
+    @Override
+    public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+        if (isChecked) {
+            if (buttonView.getId() == R.id.rb_male) {
+                rb_female.setChecked(false);
+                rb_female_male.setChecked(false);
+
+            }
+            if (buttonView.getId() == R.id.rb_female) {
+                rb_male.setChecked(false);
+                rb_female_male.setChecked(false);
+
+            }
+            if (buttonView.getId() == R.id.rb_female_male) {
+                rb_female.setChecked(false);
+                rb_male.setChecked(false);
+
+            }
+        }
     }
 }

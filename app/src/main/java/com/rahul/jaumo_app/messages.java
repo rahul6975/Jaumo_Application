243ce39a51976a561154606a12817fd8ca062e36
rahul6975 <rahulya7569@gmail.com>
2021-01-01 21:24:07 +0530

    premiums added where needed

--- app/src/main/java/com/rahul/jaumo_app/messages.java
+++ app/src/main/java/com/rahul/jaumo_app/messages.java
@@ -2,7 +2,9 @@ package com.rahul.jaumo_app;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Message;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
@@ -10,6 +12,7 @@ import android.widget.ImageView;
 public class messages extends AppCompatActivity {
 
     private Button btnback;
+    private Button btnPremium;
     private ImageView ivBack;
 
     @Override
@@ -18,6 +21,7 @@ public class messages extends AppCompatActivity {
         setContentView(R.layout.activity_messages);
 
         btnback = findViewById(R.id.btn_message_back);
+        btnPremium = findViewById(R.id.btn_message_upgrade);
         ivBack = findViewById(R.id.imageView_message);
 
         btnback.setOnClickListener(new View.OnClickListener() {
@@ -31,6 +35,14 @@ public class messages extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 finish();
+            }
+        });
+
+        btnPremium.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(messages.this,Premium_page.class);
+                startActivity(intent);
             }
         });
     }
